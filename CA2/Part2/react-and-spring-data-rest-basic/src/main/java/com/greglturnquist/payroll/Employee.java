1c14fd9b068172e734aaf269eb6d95be67541fc7
ASofiaRocha <1231867@isep.ipp.pt>
2024-04-11 12:07:05 +0100

    Close #17 add plugin

---
+++ CA2/Part2/react-and-spring-data-rest-basic/src/main/java/com/greglturnquist/payroll/Employee.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.greglturnquist.payroll;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+/**
+ * @author Greg Turnquist
+ */
+// tag::code[]
+@Entity // <1>
+public class Employee {
+
+    private @Id
+    @GeneratedValue Long id; // <2>
+    private String firstName;
+    private String lastName;
+    private String description;
+    private int jobYears;
+    private String email;
+
+
+    private Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String description, int jobYears, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.description = description;
+        this.jobYears = jobYears;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) &&
+                Objects.equals(firstName, employee.firstName) &&
+                Objects.equals(lastName, employee.lastName) &&
+                Objects.equals(description, employee.description) &&
+                Objects.equals(jobYears, employee.jobYears) &&
+                Objects.equals(email, employee.email);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, firstName, lastName, description, jobYears, email);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getJobYears() {
+        return jobYears;
+    }
+
+    public void setJobYears(int jobYears) {
+        if (jobYears < 0) {
+            throw new IllegalArgumentException("Job years must be greater than 0");
+        }
+        this.jobYears = jobYears;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (email == null || !email.contains("@")) {
+            throw new IllegalArgumentException("Email must be valid (contain '@').");
+        }
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", description='" + description + '\'' +
+                ", jobYears='" + jobYears +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+// end::code[]
---
+++ CA2/Part2/react-and-spring-data-rest-basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
@@ -0,0 +1,79 @@
+package com.greglturnquist.payroll;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @Test
+    void testGetJobYears() {
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 1, "frodo@baggins.com");
+        assertEquals(1, employee.getJobYears());
+    }
+
+    @Test
+    void testSetJobYears() {
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 1, "frodo@baggins.com");
+        employee.setJobYears(2);
+        assertEquals(2, employee.getJobYears());
+    }
+
+    @Test
+    void testCreateEmployee() {
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 1, "frodo@baggins.com");
+
+        assertEquals("Frodo", employee.getFirstName());
+        assertEquals("Baggins", employee.getLastName());
+        assertEquals(1, employee.getJobYears());
+    }
+
+
+    @Test
+    void testSetJobYearsWithNegativeValue() {
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 20, "frodo@baggins.com");
+        assertThrows(IllegalArgumentException.class, () -> {
+            employee.setJobYears(-2);
+        });
+    }
+
+    @Test
+    void testSetEmail() {
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 20, "frodo@baggins.com");
+
+        employee.setEmail("frodo@baggins.com");
+        assertEquals("frodo@baggins.com", employee.getEmail());
+    }
+
+    @Test
+    void testSetEmailWithValidEmail() {
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 20, "frodo@baggins.com");
+        employee.setEmail("frodo@baggins.com");
+        assertEquals("frodo@baggins.com", employee.getEmail());
+    }
+
+    @Test
+    void testSetEmailWithNull() {
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 20, "frodo@baggins.com");
+        assertThrows(IllegalArgumentException.class, () -> {
+            employee.setEmail(null);
+        });
+    }
+
+    @Test
+    void testSetEmailWithoutAtSymbol() {
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 20, "frodo@baggins.com");
+        assertThrows(IllegalArgumentException.class, () -> {
+            employee.setEmail("frodobaggins.com");
+        });
+    }
+
+    @Test
+    void testSetEmailWithEmptyString() {
+        Employee employee = new Employee("Frodo", "Baggins", "ring bearer", 20, "frodo@baggins.com");
+        assertThrows(IllegalArgumentException.class, () -> {
+            employee.setEmail("");
+        });
+    }
+
+}
