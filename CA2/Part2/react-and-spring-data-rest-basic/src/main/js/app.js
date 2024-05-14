00bb4b0b83c6032e19bd3489ccc5389132e37bf2
ASofiaRocha <1231867@isep.ipp.pt>
2024-05-14 23:51:25 +0100

    Close #26 added changes.

--- CA2/Part2/react-and-spring-data-rest-basic/src/main/js/app.js
+++ CA2/Part2/react-and-spring-data-rest-basic/src/main/js/app.js
@@ -9,64 +9,67 @@ const client = require('./client'); // <3>
 // tag::app[]
 class App extends React.Component { // <1>
 
-	constructor(props) {
-		super(props);
-		this.state = {employees: []};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {employees: []};
+    }
 
-	componentDidMount() { // <2>
-		client({method: 'GET', path: '/api/employees'}).done(response => {
-			this.setState({employees: response.entity._embedded.employees});
-		});
-	}
+    componentDidMount() { // <2>
+        client({method: 'GET', path: '/basic-0.0.1-SNAPSHOT/api/employees'}).done(response => {
+            this.setState({employees: response.entity._embedded.employees});
+        });
+    }
 
-	render() { // <3>
-		return (
-			<EmployeeList employees={this.state.employees}/>
-		)
-	}
+    render() { // <3>
+        return (
+            <EmployeeList employees={this.state.employees}/>
+        )
+    }
 }
+
 // end::app[]
 
 // tag::employee-list[]
-class EmployeeList extends React.Component{
-	render() {
-		const employees = this.props.employees.map(employee =>
-			<Employee key={employee._links.self.href} employee={employee}/>
-		);
-		return (
-			<table>
-				<tbody>
-					<tr>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th>Description</th>
-					</tr>
-					{employees}
-				</tbody>
-			</table>
-		)
-	}
+class EmployeeList extends React.Component {
+    render() {
+        const employees = this.props.employees.map(employee =>
+            <Employee key={employee._links.self.href} employee={employee}/>
+        );
+        return (
+            <table>
+                <tbody>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Description</th>
+                </tr>
+                {employees}
+                </tbody>
+            </table>
+        )
+    }
 }
+
 // end::employee-list[]
 
 // tag::employee[]
-class Employee extends React.Component{
-	render() {
-		return (
-			<tr>
-				<td>{this.props.employee.firstName}</td>
-				<td>{this.props.employee.lastName}</td>
-				<td>{this.props.employee.description}</td>
-			</tr>
-		)
-	}
+class Employee extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.employee.firstName}</td>
+                <td>{this.props.employee.lastName}</td>
+                <td>{this.props.employee.description}</td>
+            </tr>
+        )
+    }
 }
+
 // end::employee[]
 
 // tag::render[]
 ReactDOM.render(
-	<App />,
-	document.getElementById('react')
+    <App/>,
+    document.getElementById('react')
 )
 // end::render[]
